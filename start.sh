c4b23b123d5bc8dc736ca3226a01cdd3802c1f64
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-12-15 07:17:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Localusersbot/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Akbotmakersnew/LUCIFER-v2.git /LUCIFER-v2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /LUCIFER-v2
 fi
-cd /DQ-The-File-Donor
+cd /LUCIFER-v2
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting LUCIFER-v2...."
 python3 bot.py
