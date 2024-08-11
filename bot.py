4e2fbe3eaa64614a216f7c774eb7c850235a0c5a
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2024-08-11 18:23:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,10 +11,13 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from Script import script 
+from datetime import date, datetime 
+import pytz
 
 class Bot(Client):
 
